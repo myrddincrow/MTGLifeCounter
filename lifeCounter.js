d9db0040ca2f88d4dbce5efde39feffcf30de8f6
Samson Lancaster <myrddincrow@gmail.com>
2015-04-14 10:22:09 -0700

    Update lifeCounter.js

--- lifeCounter.js
+++ lifeCounter.js
@@ -16,6 +16,14 @@ function minusHp(){
   document.getElementById("hp").innerHTML = hpDOWN;
 }
 
+//Reset HP
+function resetHp(){
+  var currentHP = document.getElementById("hp").innerHTML;
+  document.getElementById("hp").innerHTML = 20;
+}
+
 //Run Add HP function when UP+ is clicked of Minus HP function when DOWN- is clicked
 document.getElementById("up").onclick = function () {addHp()};
 document.getElementById("down").onclick = function () {minusHp()};
+//Reset HP on click
+document.getElementById("reset").onclick = function () {resetHp()};
