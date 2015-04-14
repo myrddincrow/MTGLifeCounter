73c7df958e3a8d5e0389bdd10b0f470a07e2dcea
Samson Lancaster <myrddincrow@gmail.com>
2015-04-14 10:16:52 -0700

    Create lifeCounter.js

---
+++ lifeCounter.js
@@ -0,0 +1,21 @@
+//MTG Life Counter, starts at 20, + or - adds or subtracts life from your total
+
+//Add HP
+function addHp(){
+  var currentHP = document.getElementById("hp").innerHTML;
+  currentHP = parseFloat(currentHP);
+  var hpUP = currentHP + 1;
+  document.getElementById("hp").innerHTML = hpUP;
+
+}
+//Minus HP
+function minusHp(){
+  var currentHP = document.getElementById("hp").innerHTML;
+  currentHP = parseFloat(currentHP);
+  var hpDOWN = currentHP - 1;
+  document.getElementById("hp").innerHTML = hpDOWN;
+}
+
+//Run Add HP function when UP+ is clicked of Minus HP function when DOWN- is clicked
+document.getElementById("up").onclick = function () {addHp()};
+document.getElementById("down").onclick = function () {minusHp()};
