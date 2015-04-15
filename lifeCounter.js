fb0f36b29d20cca22c6e50f56327c47a89f93a78
Samson Lancaster <myrddincrow@gmail.com>
2015-04-15 16:42:37 -0700

    Update lifeCounter.js

--- lifeCounter.js
+++ lifeCounter.js
@@ -2,53 +2,46 @@
 //Poison Starts at 0 counters, at 10 you be dead son!
 //----DEFINE ALL THE FUNCTIONS!!-------
 //-------Life(HP) Counter-------------------
+function lifeCounter(){
 //Add HP
-function addHp(){
+document.getElementById("HPup").onclick = function (){
   var currentHP = document.getElementById("hp").innerHTML;
   currentHP = parseFloat(currentHP) + 1;
   document.getElementById("hp").innerHTML = currentHP;
-}
+};
 //Minus HP
-function minusHp(){
+document.getElementById("HPdown").onclick = function () {
   var currentHP = document.getElementById("hp").innerHTML;
   currentHP = parseFloat(currentHP) - 1;
   document.getElementById("hp").innerHTML = currentHP;
   if (currentHP == 0) {
     alert("YOU BE DEAD SON!");
   }
-}
+};
 //Reset HP
-function resetHp(){
+document.getElementById("HPreset").onclick = function (){
   document.getElementById("hp").innerHTML = 20;
 }
 //-----------Poison Counter--------------
 //Add Poison Counters
-function addPois(){
+document.getElementById("POISup").onclick = function () {
   var currentPOIS = document.getElementById("poison").innerHTML;
   currentPOIS = parseFloat(currentPOIS) + 1;
   document.getElementById("poison").innerHTML = currentPOIS;
   if (currentPOIS == 10) {
     alert("YOU BE DEAD SON!");
   }
-}
+};
 //Minus Poison Counters
-function minusPois(){
+document.getElementById("POISdown").onclick = function (){
   var currentPOIS = document.getElementById("poison").innerHTML;
   currentPOIS = parseFloat(currentPOIS) - 1;
   document.getElementById("poison").innerHTML = currentPOIS;
-}
+};
 //Reset Poison Counters
-function resetPois(){
+document.getElementById("POISreset").onclick = function (){
   document.getElementById("poison").innerHTML = 0;
 }
-//------ CALL ALL THE FUNCTIONS! --------
-//Run Add HP function when UP+ is clicked of Minus HP function when DOWN- is clicked
-document.getElementById("HPup").onclick = function () {addHp()};
-document.getElementById("HPdown").onclick = function () {minusHp()};
-//Reset HP on click
-document.getElementById("HPreset").onclick = function () {resetHp()};
-//Run Add Poison Counter function when UP+ is clicked of Minus Poison function when DOWN- is clicked
-document.getElementById("POISup").onclick = function () {addPois()};
-document.getElementById("POISdown").onclick = function () {minusPois()};
-//Reset Poison Counters on click
-document.getElementById("POISreset").onclick = function () {resetPois()};
+}
+//Ready ALL the Functions onload!
+window.onload = function() {lifeCounter()};
