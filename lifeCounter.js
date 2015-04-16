dd5b1b7f5b4cf4888203ed459a61c3f343f02ed7
Samson Lancaster <myrddincrow@gmail.com>
2015-04-15 17:07:52 -0700

    Update lifeCounter.js

--- lifeCounter.js
+++ lifeCounter.js
@@ -6,13 +6,15 @@ function lifeCounter(){
 //Add HP
 document.getElementById("HPup").onclick = function (){
   var currentHP = document.getElementById("hp").innerHTML;
-  currentHP = parseFloat(currentHP) + 1;
+  currentHP = parseFloat(currentHP);
+  currentHP++;
   document.getElementById("hp").innerHTML = currentHP;
 };
 //Minus HP
 document.getElementById("HPdown").onclick = function () {
   var currentHP = document.getElementById("hp").innerHTML;
-  currentHP = parseFloat(currentHP) - 1;
+  currentHP = parseFloat(currentHP);
+  currentHP--;
   document.getElementById("hp").innerHTML = currentHP;
   if (currentHP == 0) {
     alert("YOU BE DEAD SON!");
@@ -21,12 +23,13 @@ document.getElementById("HPdown").onclick = function () {
 //Reset HP
 document.getElementById("HPreset").onclick = function (){
   document.getElementById("hp").innerHTML = 20;
-}
+};
 //-----------Poison Counter--------------
 //Add Poison Counters
 document.getElementById("POISup").onclick = function () {
   var currentPOIS = document.getElementById("poison").innerHTML;
-  currentPOIS = parseFloat(currentPOIS) + 1;
+  currentPOIS = parseFloat(currentPOIS);
+  currentPOIS++;
   document.getElementById("poison").innerHTML = currentPOIS;
   if (currentPOIS == 10) {
     alert("YOU BE DEAD SON!");
@@ -35,13 +38,14 @@ document.getElementById("POISup").onclick = function () {
 //Minus Poison Counters
 document.getElementById("POISdown").onclick = function (){
   var currentPOIS = document.getElementById("poison").innerHTML;
-  currentPOIS = parseFloat(currentPOIS) - 1;
+  currentPOIS = parseFloat(currentPOIS);
+  currentPOIS--;
   document.getElementById("poison").innerHTML = currentPOIS;
 };
 //Reset Poison Counters
 document.getElementById("POISreset").onclick = function (){
   document.getElementById("poison").innerHTML = 0;
-}
+};
 }
 //Ready ALL the Functions onload!
 window.onload = function() {lifeCounter()};
