6500a98606ba894ab536644dab97c56bd01c0de9
Samson Lancaster <myrddincrow@gmail.com>
2015-04-17 16:31:24 -0700

    jQuery

--- lifeCounter.js
+++ lifeCounter.js
@@ -5,32 +5,102 @@
 function lifeCounter(){
 //Add HP
 //Vars for Displaying How much HP/Poison was added or subtracted
-var displayHpUp = 0,
-    displayHpDown = 0,
+var displayHpDown = 0,
     displayPoisUp = 0,
     displayPoisDown = 0,
+    display = 0,
+    current,
+    addRemove,
+    addOrSub,
     timerHpUp,
     timerHpDown,
     timerPoisUp,
     timerPoisDown;
+//Hide Minus Poison Button when Poison counters = 0
+function hidePoisDown(){
+    if ($("#poison").text() == 0){
+        $("#POISdown").addClass("hidePminus");
+    } else {
+        $("#POISdown").removeClass("hidePminus");
+    }
+}
+function addSubCounter(){
+  var currentHpPois = $(current).text();
+  if (addOrSub == "+") {
+  currentHpPois = parseInt(currentHpPois) + 1 ;
+} else if (addOrSub == "-"){
+currentHpPois = parseInt(currentHpPois) - 1 ;
+}
+  $(current).text(currentHpPois);
+  //increment the amount of HP added & write it to display
+  display++;
+  $(addRemove).text(addOrSub + display);
+}
 //Hide Minus Poison button at start
 hidePoisDown();
-document.getElementById("HPup").onclick = function (){
-  //Stop the Timer for displaying how much HP is added
-  clearTimeout(timerHpUp);
-  var currentHp = document.getElementById("hp").innerHTML;
-  currentHp = parseInt(currentHp);
-  currentHp++;
-  document.getElementById("hp").innerHTML = currentHp;
-  //increment the amount of HP added & write it to display
-  displayHpUp++;
-  document.getElementById("plusHP").innerHTML = "+" + displayHpUp;
-  //Start the timer for how much HP is added
-  timerHpUp = setTimeout(function (){displayHpUp = 0; document.getElementById("plusHP").innerHTML = "";}, 1000);
-};
+$("input:button").click(function(){
+//Add HP
+  if ( $(this).is( "input#HPup" ) ) {
+    current = "#hp";
+    addRemove = "#plusHP";
+    addOrSub = "+"
+    //Stop the Timer for displaying how much HP is added
+    clearTimeout(timerHpUp);
+    //Add or Subtract the Counter
+    addSubCounter();
+    //Start the timer for how much HP is added
+    timerHpUp = setTimeout(function (){display = 0; $(addRemove).text("");}, 1000);
+  //Minus HP
+  } else if ( $(this).is( "input#HPdown" ) ) {
+    current = "#hp";
+    addRemove = "#minusHP";
+    addOrSub = "-";
+    //Stop the Timer for displaying how much HP is added
+    clearTimeout(timerHpDown);
+    //Add or Subtract the Counter
+    addSubCounter();
+    //Start the timer for how much HP is added
+    timerHpDown = setTimeout(function (){display = 0; $(addRemove).text("");}, 1000);
+    if ($("#hp").text() == 0) {
+      alert("YOU BE DEAD SON!");
+    }
+    //Reset HP
+  } else if ( $(this).is( "input#HPreset") ) {
+    $("#hp").text("20");
+    //Add Poison
+  } else if ( $(this).is( "input#POISup" ) ) {
+    current = "#poison";
+    addRemove = "#plusPOIS";
+    addOrSub = "+";
+    //Stop the Timer for displaying how much HP is added
+    clearTimeout(timerPoisUp);
+    //Add or Subtract the Counter
+    addSubCounter();
+    //Start the timer for how much HP is added
+    timerPoisUp = setTimeout(function (){display = 0; $(addRemove).text("");}, 1000);
+    hidePoisDown();
+    if ($("#poison").text() == 10) {
+      alert("YOU BE DEAD FROM POISON SON!");
+    }
+    } else if ( $(this).is( "input#POISdown" ) ) {
+      current = "#poison";
+      addRemove = "#minusPOIS";
+      addOrSub = "-";
+      //Stop the Timer for displaying how much HP is added
+      clearTimeout(timerPoisDown);
+      //Add or Subtract the Counter
+      addSubCounter();
+      //Start the timer for how much HP is added
+      timerPoisDown = setTimeout(function (){display = 0; $(addRemove).text("");}, 1000);
+      hidePoisDown();
+    } else if ( $(this).is( "input#POISreset") ) {
+      $("#poison").text("0");
+      hidePoisDown();
+    }
+});
 
 //Minus HP
-document.getElementById("HPdown").onclick = function () {
+/*document.getElementById("HPdown").onclick = function () {
   //Stop the Timer for displaying how much HP is added
   clearTimeout(timerHpDown);
   var currentHp = document.getElementById("hp").innerHTML;
@@ -49,9 +119,7 @@ document.getElementById("HPdown").onclick = function () {
 };
 
 //Reset HP
-document.getElementById("HPreset").onclick = function (){
-  document.getElementById("hp").innerHTML = 20;
-};
+
 
 //-----------Poison Counter--------------
 //Add Poison Counters
@@ -96,15 +164,7 @@ document.getElementById("POISdown").onclick = function (){
 document.getElementById("POISreset").onclick = function (){
   document.getElementById("poison").innerHTML = 0;
   hidePoisDown();
-};
-//Hide Minus Poison Button when Poison counters = 0
-function hidePoisDown(){
-if (document.getElementById("poison").innerHTML == 0){
-  document.getElementById("POISdown").style.display = "none";
-} else {
-  document.getElementById("POISdown").style.display = "";
-}
-}
+};*/
 }
 //Ready ALL the Functions onload!
 window.onload = function() {lifeCounter()};
